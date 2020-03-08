f064eb3371690d8367d1db37477a3a55f36d3a19
Yijun Wang <yijun.wang@verizonmedia.com>
2020-03-07 21:19:53 -0600

    comments

--- import_json.py
+++ import_json.py
@@ -3,6 +3,7 @@ import requests
 import json
 import mysql.connector
 
+# curl -d "grant_type=client_credentials&client_id=w47BEjTrqDXNg0WDEqVCpmQtiYUWaIrhmg9HenATYrbSKWFmr4&client_secret=X7KbzXt1VN8TeXTOhat6LAaGvDLmrMtwNZ3a8AyW" https://api.petfinder.com/v2/oauth2/token
 api_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ3NDdCRWpUcnFEWE5nMFdERXFWQ3BtUXRpWVVXYUlyaG1nOUhlbkFUWXJiU0tXRm1yNCIsImp0aSI6ImJiNGY5MzFmN2YwNjQ1MmNlYzE5YWUyYjQ5Yjk0ZjYwOGY1Njc0NTlmZGM0ZGQyMDlkYzI2YjgzMzhhNzA5NGZlYWYyYmYxYTM5NDQxMTk2IiwiaWF0IjoxNTgzNjI5Njk3LCJuYmYiOjE1ODM2Mjk2OTcsImV4cCI6MTU4MzYzMzI5Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.u5S4omvuc0UQpeB6wjeu3iUXryI3Gq1kf1-IIvIsT82_-8BQ3nnevnbPfmy7eOnHj7keFXawbUiExp02-GW79PHVtd7UxzVwiivJf8ba56FXv1swOdxMUJ1heqEz_ACRZUsFYiLwcWkFdh2iPSrE_KrffDf8z81GXaOoUPlblqexJvJSLscQitpzc2h57l4RpKmc5A6KmY3qUrN3vUI-3SxehMhXg3tLpL77HEBaVBGSlqfcP4hR9jnAexWcuIGQYxJykwIoHgyxc3pmr9PRuCTDVg6IgpzhhOBHtiGP4kjlJSR9iHamGaE_8pgsJ4m_PO03VNKAY9Jy1DuQBKKM-Q'
 api_url_base = 'https://api.petfinder.com/v2'
 headers = {'Content-Type': 'application/json',
@@ -12,7 +13,7 @@ count = 1
 
 try:
     cnx = mysql.connector.connect(user='coasttocoast_yijun', password='sql41149.',
-                                host='localhost', database='coasttocoast_petadoptionapp')
+                                  host='localhost', database='coasttocoast_petadoptionapp')
     cursor = cnx.cursor()
 except:
     print("Log in mysql db failed!")
