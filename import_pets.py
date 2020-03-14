47d083c7a89883d655e9d6e3b62482ece377b5ae
Yijun Wang <yijun.wang@verizonmedia.com>
2020-03-13 21:24:51 -0500

    page

--- import_pets.py
+++ import_pets.py
@@ -29,8 +29,7 @@ def get_data(breed_id, breed_name):
 
     pages = [1, 2, 3, 4, 5]
     for page in pages:
-        #api_url = api_url_base + '/animals?breed=' + breed_name + '\&page=' + str(page)
-        api_url = 'https://api.petfinder.com/v2/animals?breed=Affenpinscher\&page=1'
+        api_url = api_url_base + '/animals?breed=' + breed_name + '&page=' + str(page)
         print(api_url)
         response = requests.get(api_url, headers=headers)
         if response.status_code == 200:
