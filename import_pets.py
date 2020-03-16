e62a23eb2ef525ef761a7db49f2eb8f051be9337
Dargooo <32312234+dargooo@users.noreply.github.com>
2020-03-16 10:47:59 -0500

    Yijunw4 (#4)
    * generate age, weight, color, hair, personality
    Co-authored-by: Yijun Wang <yijun.wang@verizonmedia.com>

--- import_pets.py
+++ import_pets.py
@@ -2,13 +2,15 @@ import sys
 import requests
 import json
 import mysql.connector
+from random import seed
+from random import random
+from random import choice
 
 # curl -d "grant_type=client_credentials&client_id=w47BEjTrqDXNg0WDEqVCpmQtiYUWaIrhmg9HenATYrbSKWFmr4&client_secret=X7KbzXt1VN8TeXTOhat6LAaGvDLmrMtwNZ3a8AyW" https://api.petfinder.com/v2/oauth2/token
-api_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ3NDdCRWpUcnFEWE5nMFdERXFWQ3BtUXRpWVVXYUlyaG1nOUhlbkFUWXJiU0tXRm1yNCIsImp0aSI6IjMzNGNjMDhmMTcyOGExODAwYzc5Y2IzN2NlN2ZhOTZmMzJlZGE4MmE1NWZjY2ExYmNkMzQyMTFhMWI3N2M1ODk5MjU0ZTkxMDk0YzU3Yzk1IiwiaWF0IjoxNTgzNjU3Nzc1LCJuYmYiOjE1ODM2NTc3NzUsImV4cCI6MTU4MzY2MTM3NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.g40ewlW34YynSAEMrn4cBHoBQ3eJc2kbtsaS8ncBK9MKiXu8qVa5V8wVGQlq1cCqO9EGs-vRG0ud9UsqVovc3n9OPyAWDR-qt-nX1KfoAdxhKJr-rGMejzLvHxvo9y6jjX2xmxfTgOxeeloACiSkAEaLQNappHiYVCKT1Z3xAaOtvdkC27AUf8BsQsWPYlUDqWdaTJYbJshcASn-ZVhbq-knHb6_8yIuG5t8dxCCsCzgGv-pnXr6EMuB-jrgqEmMttG-JpJ_1diWf1kW5hlhLfAKvSqqHbWqS-vCSqkZoHDPx5EbxsnaquLRMemjyOi7iz8d6ZNguz9SZUB6vQadJw'
+api_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ3NDdCRWpUcnFEWE5nMFdERXFWQ3BtUXRpWVVXYUlyaG1nOUhlbkFUWXJiU0tXRm1yNCIsImp0aSI6Ijg2ODM3NjcxZjE3MDU1Y2I3Y2RlNWJhNzRhMjY0ZGRlNDIxYzJjNDU5ZTFlOWVlYjQzYmUxZTg5ZDY5MjMzNWJhZDgzMjE3NTQwN2UxYTAzIiwiaWF0IjoxNTg0MzcxNTk1LCJuYmYiOjE1ODQzNzE1OTUsImV4cCI6MTU4NDM3NTE5NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.PceYF7nOAdUfOaX5n1wR4FgEB2UDcFaSXiotcsThcX0o0xDt7u-FnjstLRFYYLIBKYJALZxdYHCrLawca883LNMMCyoc2aNj8LU9P2Lm0ovq7zKYeg4cxIOJvlwKPZpzhJov4LMHQJE5xLKW79ZoMUt7WGpcK0w4H5cF376HRL0MNTVQOCR4-mAp7-ccnVGYo0lMTvlktj7vkRT_qGl4F8OaYbeMf6tok17LArqj7VDVBwUc2bkKAyyuEHURKeDsos3xSI1vAOqQVjlsidTynyCFmrTaTUVbSD2J5QsHoJXDU4K7LMBn-UAbj_wV9IGKGaA0h0-r0pZZP-x3Zb9sjg'
 api_url_base = 'https://api.petfinder.com/v2'
 headers = {'Content-Type': 'application/json',
            'Authorization': 'Bearer {0}'.format(api_token)}
-breeds = ['cockapoo', 'Siberian','tiger', 'Persian']
 count = 1
 
 try:
@@ -18,35 +20,63 @@ try:
 except:
     print("Log in mysql db failed!")
 
-
-def get_data(i):
+def get_data(breed_id, breed_name):
     global count
-    x = breeds[i]
-    api_url = api_url_base + '/animals?breed=' + x
-    print(api_url)
-    response = requests.get(api_url, headers=headers)
-    if response.status_code == 200:
-        pet_list = json.loads(response.content.decode('utf-8'))['animals']
-        print(len(pet_list))
-        insert_query = "INSERT INTO pets VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        for x in pet_list:
-            c = x['colors']
-            g = x['gender']
-            photo_list = x['photos']
-            photo = "NULL"
-            if (len(photo_list) > 0):
-                photo = photo_list[0]['full']
-            t = x['tags']
-            query_data = (count, x['name'], 2.5, g[0], 7.7, x['status'], ','.join(t), c['primary'], photo, x['coat'], 76, "NULL", "NULL")
-            count += 1
-            cursor.execute(insert_query, query_data)
-    else:
-        print("Failed to connect to " + api_url)
+
+    pages = [1, 2, 3, 4, 5]
+    for page in pages:
+        api_url = api_url_base + '/animals?breed=' + breed_name + '&page=' + str(page)
+        print(api_url)
+        response = requests.get(api_url, headers=headers)
+        if response.status_code == 200:
+            seed(1)
+            pet_list = json.loads(response.content.decode('utf-8'))['animals']
+            print(len(pet_list))
+
+            insert_query = "INSERT INTO pet VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            for x in pet_list:
+    
+                age = round(random() * 15, 1)
+    
+                gender = x['gender']
+    
+                if age < 1:
+                    weight = round(age * (3.5 + random()) + 2, 1)
+                else:
+                    weight = round(age * (0.8 + random() * 0.4) + 5, 1)
+    
+                personality_list = ['sweet', 'stubborn', 'proud', 'quite', 'cute', 'rotal', 'independent', 'happy', 'shy', 'adaptable', 'intelligent', 'anxious', 'brave', 'crazy', 'wild', 'needy']
+                personality = choice(personality_list)
+    
+                color_list = ['white', 'brown', 'black', 'orange', 'gray', 'mix']
+                color = choice(color_list)
+    
+                photo_list = x['photos']
+                photo = "NULL"
+                if (len(photo_list) > 0):
+                    photo = photo_list[0]['full']
+    
+                hair_list = ['none', 'short', 'medium', 'long']
+                hair = x['coat']
+                if not hair:
+                    hair = choice(hair_list)
+    
+                query_data = (count, x['name'], age, gender[0], weight, x['status'], personality, color, photo, hair, breed_id, "NULL", "NULL")
+                count += 1
+                cursor.execute(insert_query, query_data)
+            
+            if len(pet_list) < 20:
+                break
+        else:
+            print("Failed to connect to " + api_url)
 
 
 def main(argv):
-    for i in range(0, len(breeds)):
-        get_data(i)
+    select_query = "SELECT id AS breed_id, name AS breed_name FROM breed"
+    cursor.execute(select_query)
+    result = cursor.fetchall()
+    for (breed_id, breed_name) in result:
+        get_data(breed_id, breed_name)
     cnx.commit()
     cursor.close()
     cnx.close()
