e82e65d6343ef3b45cbe9f96d037c64b49bf54e4
Yijun Wang <yijun.wang@verizonmedia.com>
2020-03-18 19:38:29 -0500

    init_user

---
+++ init_user.py
@@ -0,0 +1,48 @@
+import sys
+import csv
+import mysql.connector
+from random import randint
+
+try:
+#    cnx = mysql.connector.connect(user='coasttocoast_yijun', password='sql41149.',
+    cnx = mysql.connector.connect(user='root', password='Ms41149.',
+                                  host='localhost', database='coasttocoast_petadoptionapp')
+    cursor = cnx.cursor()
+except:
+    print("Log in mysql db failed!")
+
+u_reader = open("datafile/38650-username-sktorrent.txt", "rb")
+p_reader = open("datafile/38650-password-sktorrent.txt", "rb")
+n_reader = open("datafile/facebook-names.txt", "rt")
+
+def init_user(i):
+    insert_query = "INSERT INTO user VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    username = u_reader.readline().split()[0]
+    password = p_reader.readline().split()[0]
+
+    firstname = n_reader.readline().split()[0]
+    lastname  = n_reader.readline().split()[0]
+    name = firstname + ' ' + lastname
+
+    # possibility - person : shelter = 5 : 1
+    num = randint(1, 7)
+    if (num < 6):
+        is_person = True
+    else:
+        is_person = False
+    
+    query_data = (username, password, name, None, None, None, None, None, is_person)
+    cursor.execute(insert_query, query_data)
+
+
+def main(argv):
+    for i in range(1, 20):
+        init_user(i)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+ 
+
+if __name__=="__main__":
+    main(sys.argv)
