7388514b2578a5f0a9364160b0837f9d00449597
Yijun Wang <yijun.wang@verizonmedia.com>
2020-03-19 23:33:51 -0500

    printable

--- init_user.py
+++ init_user.py
@@ -22,11 +22,9 @@ def init_user(i):
     username = u_reader.readline().split()[0]
     password = p_reader.readline().split()[0]
     for c in username: 
-        if c not in string.printable: 
-            return
+        if not chr(c) in string.printable: return
     for c in password: 
-        if c not in string.printable: 
-            return
+        if not chr(c) in string.printable: return
 
 
     num = randint(1, 101)
