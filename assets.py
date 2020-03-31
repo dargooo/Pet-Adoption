73a56e490caba3b4770858dec0e14d668c65f4b6
Yijun Wang <yijun.wang@verizonmedia.com>
2020-03-30 23:05:15 -0500

    clickable

--- assets.py
+++ assets.py
@@ -2,6 +2,9 @@ from flask_assets import Bundle, Environment
 from main import app
 
 bundles = {
+    'search_js': Bundle(
+        'js/search.js',
+        output='gen/search.js'),
     'present_css': Bundle(
         'css/present.css',
         output='gen/present.css'),
