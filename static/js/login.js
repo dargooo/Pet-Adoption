d4b8a4186070602d76dc89d0a2e72b5a13549043
Yijun Wang <yijun.wang@verizonmedia.com>
2020-04-09 22:17:52 -0500

    login

---
+++ static/js/login.js
@@ -0,0 +1,27 @@
+function login() {
+    var site = 'http://coasttocoast.web.illinois.edu';
+
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+    // user info
+    var url = site + '/user?username=\"' + username + '\"';
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(obj => {
+			if (obj.password == password) {
+				alert("success!");
+			// login success
+			}
+			else { alert("Username or password wrong !"); }
+        });
+    })
+    .catch(error => console.log('ERROR'));
+
+	alert("Username does not exist !");
+}
+
+document.getElementById('btn-login').addEventListener('click', function(){ login(); }, false);
