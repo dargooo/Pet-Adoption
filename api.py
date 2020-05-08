8c4ead4064dbc7275fc0e995bb1d0dfa95325e56
Yijun Wang <yijun.wang@verizonmedia.com>
2020-05-08 15:40:49 -0500

    dup cat

--- api.py
+++ api.py
@@ -48,7 +48,7 @@ class Pet(Resource):
         args = parser.parse_args()
         print(args)
         query = "SELECT *, pet.name AS name, breed.name AS breed FROM breed, pet WHERE breed.id = pet.breed_id AND breed_id IN (SELECT id FROM breed WHERE id >= 0)"
-        if args['species_id']:  query = query + " ANDspecies_id = " + str(args['species_id'])
+        if args['species_id']:  query = query + " AND species_id = " + str(args['species_id'])
         if args['id']:          query = query + " AND pet.id = " + str(args['id'])
         if args['min_age']:     query = query + " AND age >= " + str(args['min_age'])
         if args['max_age']:     query = query + " AND age <= " + str(args['max_age'])
